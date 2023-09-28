d06d57de6a00884f3e161d803312d25206c6d93a
freddieDevs <133527315+freddieDevs@users.noreply.github.com>
2023-09-28 10:22:32 +0300

    trial 1 vercel

---
+++ src/index.ts
@@ -0,0 +1,27 @@
+import { NestFactory } from '@nestjs/core';
+import type { VercelRequest, VercelResponse } from '@vercel/node';
+import { ExpressAdapter } from '@nestjs/platform-express';
+import { AppModule } from './app.module';
+import * as express from 'express';
+
+const server = express();
+
+export const createNestServer = async (expressInstance) => {
+  const app = await NestFactory.create(
+    AppModule,
+    new ExpressAdapter(expressInstance),
+  );
+
+  return app.init();
+};
+
+createNestServer(server)
+  .then(() => console.log('Nest Ready'))
+  .catch((err) => console.error('Nest broken', err));
+
+// be able to export this function exposed
+
+export const handler = async (req: VercelRequest, res: VercelResponse) => {
+  await createNestServer(server);
+  server(req, res);
+};
